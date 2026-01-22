9c3724a4fe4f5115b2edc9a098b3cf1d93d8dc8f
azadnio <azadnio@gmail.com>
2026-01-22 23:24:48 +0000

    Deploying to gh-pages from @ azadnio/ecommerce@3c212a00864adab964e17d3816e97e24c2eaa990 🚀

---
+++ chunk-MBOJPP77.js
@@ -0,0 +1,17 @@
+import{Fb as a,Za as e,fa as s,mc as c,oc as l,xa as t,ya as n}from"./chunk-4VOC2XLA.js";var r=class o{static \u0275fac=function(i){return new(i||o)};static \u0275cmp=s({type:o,selectors:[["app-order-success"]],decls:22,vars:0,consts:[[1,"flex","items-center","justify-center","h-[calc(100vh-64px)]","py-6"],[1,"flex","flex-col","items-center","justify-center","text-center","bg-white","rounded-xl","shadow","p-8","gap-6","max-w-sm"],[1,"!text-green-500","!h-[56px]","!w-[56px]","!text-[56px]"],[1,"font-semibold","text-green-600","text-2xl","font-bold"],[1,"text-base"],[1,"text-gray-600"],["matButton","filled","routerLink","/","color","primary",1,"w-full","max-w-xs","mt-2"]],template:function(i,d){i&1&&(e(0,`
+    `),t(1,"div",0),e(2,`
+      `),t(3,"div",1),e(4,`
+        `),t(5,"mat-icon",2),e(6,"check_circle"),n(),e(7,`
+        `),t(8,"h2",3),e(9,"Order successful!"),n(),e(10,`
+        `),t(11,"p",4),e(12,`
+          Thank you for your purchase! 
+          Your order has been confirmed                     and will be shipped soon.`),n(),e(13,`
+        `),t(14,"p",5),e(15,`
+          You will receive an email confirmation shortly with the details of your order. and tracking information once your items have shipped.
+        `),n(),e(16,`
+        `),t(17,"a",6),e(18,`
+          Continue Shopping
+        `),n(),e(19,`
+      `),n(),e(20,`
+    `),n(),e(21,`
+  `))},dependencies:[c,l,a],encapsulation:2})};export{r as default};
