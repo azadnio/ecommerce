74bc302452986bbc1dcf0a9423819d53086d6b87
azadnio <azadnio@gmail.com>
2026-01-23 22:17:54 +0000

    Deploying to gh-pages from @ azadnio/ecommerce@9b463b1a5f59ae6be6baa43e434b538f0df05df4 🚀

---
+++ chunk-FJTWGBEK.js
@@ -0,0 +1,1 @@
+import{ia as t}from"./chunk-FOS54PHM.js";var e=class r{constructor(){}static \u0275fac=function(i){return new(i||r)};static \u0275dir=t({type:r,selectors:[["","appViewPanal",""]],hostAttrs:[1,"border","border-gray-200","rounded-xl","p-6","bg-white"]})};export{e as a};
