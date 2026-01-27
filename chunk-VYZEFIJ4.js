a3a4bf7e01a066e07ea9d4484de67a7cb13a6931
azadnio <azadnio@gmail.com>
2026-01-27 03:00:18 +0000

    Deploying to gh-pages from @ azadnio/ecommerce@51319256c6510010a33f5b46c72edc6bd7bc1b54 🚀

--- chunk-VYZEFIJ4.js
+++
@@ -1,1 +0,0 @@
-import{ma as t}from"./chunk-36KNVKLJ.js";var e=class r{constructor(){}static \u0275fac=function(i){return new(i||r)};static \u0275dir=t({type:r,selectors:[["","appViewPanal",""]],hostAttrs:[1,"border","border-gray-200","rounded-xl","p-6","bg-white"]})};export{e as a};
